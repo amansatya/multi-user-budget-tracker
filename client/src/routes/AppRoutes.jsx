50a6470c8ce81300dd02b2745f5b0489dda3aa04
SATYA AMAN <satyaaman1012@gmail.com>
2025-07-22 00:15:43 +0530

    Setup routing for all main pages

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import DayView from "../pages/DayView";
+import Settings from "../pages/Settings";
+import NotFound from "../pages/NotFound";
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/day/:date" element={<DayView />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
