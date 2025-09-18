fec5977d140645233dbe4ea67666557c889c26a3
SATYA AMAN <satyaaman1012@gmail.com>
2025-09-19 01:09:12 +0530

    Updated User model with password hashing (bcrypt) + matchPassword method

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema=new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
         name: {
             type: String,
             required: true,
@@ -31,6 +33,17 @@ const userSchema=new mongoose.Schema({
         timestamps: true,
     }
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
